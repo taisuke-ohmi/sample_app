478e0983b37537b9fe9f7ff055f4e27c97ae3575
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-07-28 14:55:30 +0000

    add login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,12 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      redirect_to 'new'
     end
   end
   
