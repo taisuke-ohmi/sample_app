f0b5694bcd95d10b0292c3ee10256d1fd60da79e
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-07-29 10:52:22 +0000

    add remember me check box

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
-      redirect_to 'new'
+      render 'new'
     end
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
