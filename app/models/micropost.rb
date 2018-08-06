b1354c18b14988cc85d3cac47c374da6d5640777
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-06 15:59:47 +0000

    add upload validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -4,4 +4,14 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: { maximum: 140 }
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
+  validate  :picture_size
+
+  private
+
+    # アップロードされた画像のサイズをバリデーションする
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end
 end
