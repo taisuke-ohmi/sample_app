d4cae61416e0a50f33008e046e155c6383caff2d
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-06 16:23:22 +0000

    add follow and follower count

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,12 @@ Rails.application.routes.draw do
   get '/signup', to:'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'  
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
