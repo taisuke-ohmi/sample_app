821af8df7029fdb38f4b25dbc2dff1026c8acda4
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-06 16:17:45 +0000

    add relationships test

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:   :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
