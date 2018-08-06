6896361ed1b5f65b7bff242804f85f12618bd004
taisuke-ohmi <taisukeohmi0420@gmail.com>
2018-08-06 15:03:42 +0000

    add micropost test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -54,18 +54,4 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_not duplicate_user.valid?
   end
-
-  test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
-    assert_not @user.valid?
-  end
-
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
-    assert_not @user.valid?
-  end
-
-  test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?(:remember, '')
-  end
 end
